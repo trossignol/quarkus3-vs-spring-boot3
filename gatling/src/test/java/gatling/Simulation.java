a20e1efd9a66b3499ff7c0b6c65c0674cb0a585d
Thomas Rossignol <thomas.rossignol@decathlon.com>
2022-12-26 15:14:29 +0100

    Add gatling

---
+++ gatling/src/test/java/gatling/Simulation.java
@@ -0,0 +1,24 @@
+package gatling;
+
+import static io.gatling.javaapi.core.CoreDsl.rampUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+public class Simulation extends io.gatling.javaapi.core.Simulation {
+
+        HttpProtocolBuilder httpProtocol = http
+                        .baseUrl("http://localhost:8080")
+                        .acceptHeader("application/json");
+
+        ScenarioBuilder scn = scenario("Scenario")
+                        .exec(http("request_1")
+                                        // .get("/api/thomas/async"))
+                                        .get("/fruit/id/1"));
+
+        {
+                setUp(scn.injectOpen(rampUsersPerSec(1).to(30).during(30)).protocols(httpProtocol));
+        }
+}
