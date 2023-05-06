65a5b6f16289be6fcfd3cb9e0246dd8998c854f9
Thomas Rossignol <thomas.rossignol@decathlon.com>
2023-05-06 12:35:38 +0200

    kotlin smarter

--- label-aggregator-springboot3-kotlin/src/main/kotlin/com/example/aggregator/LabelClient.kt
+++ label-aggregator-springboot3-kotlin/src/main/kotlin/com/example/aggregator/LabelClient.kt
@@ -9,31 +9,12 @@ import org.springframework.web.reactive.function.client.awaitExchange
 
 @Service
 class LabelClient(private val client: WebClient) {
-    @Value("\${label-client.url}")
-    private lateinit var baseURL: String
+    @Value("\${label-client.url}") private lateinit var baseURL: String
 
     suspend fun getAsync(key: String): LabelResult =
-        client.get().uri("$baseURL/$key").awaitExchange { clientResponse ->
-            clientResponse.awaitBody<LabelResult>()
-        }
-
-
-    suspend fun getAsyncAll(key: String, nb: Int = 3): List<String> {
-        val results = mutableListOf<String>()
-        CoroutineScope(Dispatchers.Default).run {
-            results.addAll((1..nb).map {
-                async {
-                    getOneLabel(key, it)
-                }
-            }.awaitAll())
-        }
-        return results
-    }
-
-    private suspend fun getOneLabel(key: String, it: Int): String =
-        client.get().uri("$baseURL/${key + it}").awaitExchange { clientResponse ->
-            clientResponse.awaitBody<LabelResult>().label
-        }
+            client.get().uri("$baseURL/${key}").awaitExchange { clientResponse ->
+                clientResponse.awaitBody<LabelResult>()
+            }
 
     data class LabelResult(val key: String, val label: String)
 }
--- label-aggregator-springboot3-kotlin/src/main/kotlin/com/example/aggregator/Resource.kt
+++ label-aggregator-springboot3-kotlin/src/main/kotlin/com/example/aggregator/Resource.kt
@@ -1,5 +1,6 @@
 package com.example.aggregator
 
+import kotlinx.coroutines.*
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
@@ -7,26 +8,17 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class Resource(private val labelClient: LabelClient) {
-    @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
 
     @GetMapping("/api/{key}/async")
-    suspend fun getLabels(
-        @PathVariable("key") key: String, @RequestParam(value = "nb", defaultValue = "3") nb: Int
+    suspend fun getAsync(
+            @PathVariable("key") key: String,
+            @RequestParam(value = "nb", defaultValue = "3") nb: Int
     ): Result {
-        val labels: MutableList<String> = mutableListOf()
-        labels.addAll((1..nb).map { i ->
-            labelClient.getAsync(key + i).label
-        })
-
-        return Result(key, labels)
-    }
-
-    @GetMapping("/api/{key}/asyncall")
-    suspend fun getLabelsAll(
-        @PathVariable("key") key: String, @RequestParam(value = "nb", defaultValue = "3") nb: Int
-    ): Result {
-        return Result(key, labelClient.getAsyncAll(key, nb))
+        val scope = CoroutineScope(Dispatchers.Default)
+        return Result(
+                key,
+                (1..nb).map { scope.async { labelClient.getAsync(key + it).label } }.awaitAll()
+        )
     }
 
     data class Result(val key: String, val labels: List<String>)
